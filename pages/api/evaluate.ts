c30af20fd719bc2223f03c8b552ce7cda34cfade
Muqsit <hackhack05@gmail.com>
2023-04-02 03:43:28 +0500

    Add validation for SSR /api/evaluate

--- pages/api/evaluate.ts
+++ pages/api/evaluate.ts
@@ -1,11 +1,37 @@
-import { NextRequest } from "next/server"
-import { env } from "process";
+import type { NextApiRequest } from "next";
 
 export const config = {
 	runtime: "edge",
 }
 
-export default async function handler(req: NextRequest){
-	const params = new URLSearchParams(req.nextUrl.searchParams);
-	return fetch(`${process.env.ARITHMEXP_EVALUATOR}?${params}`, {method: "GET"});
+export default function handler(req: NextApiRequest){
+	const params = new URL(req.url ?? "").searchParams;
+
+	const expression = params.get("expression");
+	const parser = params.get("parser");
+	const variables = params.get("variables");
+	if(expression === null || parser === null || variables === null){
+		return new Response("Insufficient number of parameters were supplied", {status: 400});
+	}
+
+	let vars;
+	try{
+		vars = JSON.parse(variables);
+	}catch(_){
+		return new Response("Improperly formatted value for 'variables' was supplied", {status: 400});
+	}
+	if(typeof vars !== "object"){
+		return new Response("Improper structure for 'variables' was supplied", {status: 400});
+	}
+	for(const key in vars){
+		if(typeof vars[key] !== "number"){
+			return new Response("Improper value types for 'variables' was supplied", {status: 400});
+		}
+	}
+
+	return fetch(`${process.env.ARITHMEXP_EVALUATOR}?${new URLSearchParams({
+		expression: expression,
+		parser: parser,
+		variables: variables
+	})}`, {method: "GET"});
 }
