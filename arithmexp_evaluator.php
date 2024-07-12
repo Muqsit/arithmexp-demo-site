e795a9484a4f8a0fdfe7c38ecbfd5f5949644c25
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2024-07-12 17:55:02 +0800

    Add support for boolean data type

--- arithmexp_evaluator.php
+++ arithmexp_evaluator.php
@@ -149,7 +149,8 @@ echo json_encode([
 		"result" => [
 			"type" => match(gettype($result)){
 				"integer" => "int",
-				"double" => "float"
+				"double" => "float",
+				"boolean" => "boolean"
 			},
 			"value" => (string) $result // because $result can be INF/NAN which are not supported in JSON
 		],
